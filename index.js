5e7aefd014df0b60a01d325fd62b770219083d37
Hooman <68755769+HoomanBud@users.noreply.github.com>
2022-11-04 23:20:37 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require("express")
 const session = require("express-session")
 
 const app = express()
+
+app.get("/", function(req, res) {
+    res.sendStatus(200)
+})
+
+app.listen(80)
